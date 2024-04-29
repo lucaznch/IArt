207b025d3009a33776947c986493210df5d16355
lucaznch <lucazanchisilva@gmail.com>
2024-04-29 17:36:38 +0100

    new representation of state: list of lists with Pieces (objects) instead of strings

--- pipe.py
+++ pipe.py
@@ -4,7 +4,6 @@
 
 from sys import stdin
 
-
 from search import (
     Problem,
     Node,
@@ -14,6 +13,184 @@ from search import (
     greedy_search,
     recursive_best_first_search,
 )
+
+
+# Piece types:
+PIECE_TYPE_F = 'F' # Closed piece
+PIECE_TYPE_B = 'B' # Forked piece
+PIECE_TYPE_V = 'V' # Turning piece
+PIECE_TYPE_L = 'L' # Connection piece
+
+# Piece orientations:
+PIECE_ORIENTATION_UP = 'C'
+PIECE_ORIENTATION_DOWN = 'B'
+PIECE_ORIENTATION_LEFT = 'E'
+PIECE_ORIENTATION_RIGHT = 'D'
+
+# Piece rotations:
+PieceRotations = {'FC': ['E', 'D'],
+                  'FB': ['D', 'E'],
+                  'FE': ['B', 'C'],
+                  'FD': ['C', 'B'],
+                  'BC': ['E', 'D'],
+                  'BB': ['D', 'E'],
+                  'BE': ['B', 'C'],
+                  'BD': ['C', 'B'],
+                  'VC': ['E', 'D'],
+                  'VB': ['D', 'E'],
+                  'VE': ['B', 'C'],
+                  'VD': ['C', 'B'],
+                  'LH': ['V', 'V'],
+                  'LV': ['H', 'H']}
+
+# Piece connections:
+PieceConnections = {'FC': ['BB', 'BE', 'BD', 'VB', 'VE', 'LV'],
+                    'FB': ['D', 'E'],
+                    'FE': ['B', 'C'],
+                    'FD': ['C', 'B'],
+                    'BC': ['E', 'D'],
+                    'BB': ['D', 'E'],
+                    'BE': ['B', 'C'],
+                    'BD': ['C', 'B'],
+                    'VC': ['E', 'D'],
+                    'VB': ['D', 'E'],
+                    'VE': ['B', 'C'],
+                    'VD': ['C', 'B'],
+                    'LH': ['V', 'V'],
+                    'LV': ['H', 'H']}
+
+
+
+
+class Piece:
+    def __init__(self, piece):
+        self.type = piece[0]
+        self.orientation = piece[1]
+        self.piece = piece
+    
+    def get_type(self):
+        return self.type
+    
+    def get_orientation(self):
+        return self.orientation
+
+    def get_piece(self):
+        return self.piece
+    
+    def set_orientation(self, orientation):
+        self.orientation = orientation
+
+    def rotate(self, way):
+        """ way: 0 for anti clockwise, 1 for clockwise"""
+        new_orientation = PieceRotations[self.piece][way]
+        self.orientation = new_orientation
+        new_piece = self.type + new_orientation
+        self.piece = new_piece
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class Board:
+    """Internal representation of a PipeMania board."""
+
+    def __init__(self, board, len):
+        self.board = board
+        self.len = len
+
+    def get_value(self, row: int, col: int) -> str:
+        """Returns the value at the respective position on the board."""
+        return self.board[row][col].get_piece()
+
+    def get_len(self):
+        return self.len
+
+    def get_board(self):
+        """Returns the board. The board is a list of lists. Each list represents a row of the board."""
+        # example: [[Piece1, Piece2], [Piece3, Piece4]]
+        return self.board
+    
+    def get_board_display(self):
+        for i in range(self.len):
+            for j in range(self.len):
+                if j == self.len - 1:
+                    print(self.board[i][j].get_piece())
+                else:
+                    print(self.board[i][j].get_piece() + "\t", end='')
+
+    def adjacent_vertical_values(self, row: int, col: int) -> tuple[str, str]:
+        """Returns the values immediately above and below, respectively."""
+        if row == 0 and row != self.len - 1:
+            above = "None"
+            below = self.board[row + 1][col].get_piece()
+
+        elif row != 0 and row == self.len - 1:
+            above = self.board[row - 1][col].get_piece()
+            below = "None"
+        
+        elif row == 0 and row == self.len - 1:
+            above = "None"
+            below = "None"
+        
+        else:
+            above = self.board[row - 1][col].get_piece()
+            below = self.board[row + 1][col].get_piece()
+
+        return "(" + above + ", " + below + ")"
+
+    def adjacent_horizontal_values(self, row: int, col: int) -> tuple[str, str]:
+        """Returns the values immediately to the left and right, respectively."""
+        if col == 0 and col != self.len - 1:
+            left = "None"
+            right = self.board[row][col + 1].get_piece()
+
+        elif col != 0 and col == self.len - 1:
+            left = self.board[row][col - 1].get_piece()
+            right = "None"
+        
+        elif col == 0 and col == self.len - 1:
+            left = "None"
+            right = "None"
+        
+        else:
+            left = self.board[row][col - 1].get_piece()
+            right = self.board[row][col + 1].get_piece()
+
+        return "(" + left + ", " + right + ")"
+
+    def rotate_one_piece(self, row, col, way):
+        self.board[row][col].rotate(way)
+
+    @staticmethod
+    def parse_instance():
+        n = 0
+        pieces = []
+
+        while True:
+            line = stdin.readline().split()
+            if not line:
+                break
+            n += 1
+            pieces.append([Piece(piece) for piece in line])
+
+        return Board(pieces, n)
+
+
+
+
+
 
 
 class PipeManiaState:
@@ -30,89 +207,6 @@ class PipeManiaState:
     # TODO: more methods?
 
 
-class Board:
-    """Internal representation of a PipeMania board."""
-
-    def __init__(self, board, len):
-        self.board = board
-        self.len = len
-
-    def get_value(self, row: int, col: int) -> str:
-        """Returns the value at the respective position on the board."""
-        # TODO
-        pass
-
-    def get_len(self):
-        return self.len
-
-    def get_board(self):
-        """Returns the board. The board is a list of lists. Each list represents a row of the board."""
-        # example: [['VB', 'VE'], ['FC', 'FC']]
-        return self.board
-    
-    def get_board_display(self):
-        for i in range(self.len):
-            for j in range(self.len):
-                if j == self.len - 1:
-                    print(self.board[i][j])
-                else:
-                    print(self.board[i][j] + "\t", end='')
-        
-
-    def adjacent_vertical_values(self, row: int, col: int) -> tuple[str, str]:
-        """Returns the values immediately above and below, respectively."""
-        if row == 0 and row != self.len - 1:
-            above = "None"
-            below = self.board[row + 1][col]
-
-        elif row != 0 and row == self.len - 1:
-            above = self.board[row - 1][col]
-            below = "None"
-        
-        elif row == 0 and row == self.len - 1:
-            above = "None"
-            below = "None"
-        
-        else:
-            above = self.board[row - 1][col]
-            below = self.board[row + 1][col]
-
-
-        return "(" + above + ", " + below + ")"
-
-    def adjacent_horizontal_values(self, row: int, col: int) -> tuple[str, str]:
-        """Returns the values immediately to the left and right, respectively."""
-        if col == 0 and col != self.len - 1:
-            left = "None"
-            right = self.board[row][col + 1]
-
-        elif col != 0 and col == self.len - 1:
-            left = self.board[row][col - 1]
-            right = "None"
-        
-        elif col == 0 and col == self.len - 1:
-            left = "None"
-            right = "None"
-        
-        else:
-            left = self.board[row][col - 1]
-            right = self.board[row][col + 1]
-
-
-        return "(" + left + ", " + right + ")"
-
-    @staticmethod
-    def parse_instance():
-        n = 0
-        matrix = []
-
-        while True:
-            line = stdin.readline().split()
-            if not line:
-                break
-            matrix.append(line)
-
-        return Board(matrix, len(matrix))
 
 
 
@@ -126,6 +220,7 @@ class PipeMania(Problem):
 
     def actions(self, state: PipeManiaState):
         """Returns a list of actions that can be performed from the state passed as an argument."""
+        state.board
         # TODO
         pass
 
@@ -138,6 +233,9 @@ class PipeMania(Problem):
     def goal_test(self, state: PipeManiaState):
         """Returns True if and only if the state passed as an argument is an objective state.
         You must check that all positions on the board are filled according to the rules of the problem."""
+        
+        
+        
         # TODO
         pass
 
@@ -149,6 +247,16 @@ class PipeMania(Problem):
     # TODO: more methods?
 
 
+
+
+
+
+
+
+
+
+
+
 if __name__ == "__main__":
     # TODO:
     # Read the standard input file,
@@ -156,15 +264,51 @@ if __name__ == "__main__":
     # Remove the solution from the resulting node,
     # Print to standard output in the indicated format.
 
+
+    # Read the grid in figure 1a:
     board = Board.parse_instance()
 
-    print("\n")
-    board.get_board_display()
-    print("\n")
-    print(board.get_board())
-    print("\n")
+    # Create an instance of PipeMania:
+    problem = PipeMania(board)
 
+    # Create a state with the initial configuration:
+    initial_state = PipeManiaState(board)
+
+
+    print("initial state:")
+    board.get_board_display()
+
+
+    print("piece at (2,2)")
+    print(initial_state.board.get_value(2, 2))
+
+
+    print("\nadjacent vertical values of 0,0")
     print(board.adjacent_vertical_values(0, 0))
+    print("\nadjacent horizontal values of 0,0")
     print(board.adjacent_horizontal_values(0, 0))
+
+    print("\nadjacent vertical values of 1,1")
     print(board.adjacent_vertical_values(1, 1))
+    print("\nadjacent horizontal values of 1,1")
     print(board.adjacent_horizontal_values(1, 1))
+
+    print("\nrotating piece at (0,0) clockwise(=1)")
+    print("rotating piece at (1,1) anticlockwise(=0)")
+    print("rotating piece at (0,1) clockwise(=1)")
+    print("rotating piece at (1,0) anticlockwise(=0)\n")
+
+    board.rotate_one_piece(0, 0, 1)
+    board.rotate_one_piece(1, 1, 0)
+    board.rotate_one_piece(0, 1, 1)
+    board.rotate_one_piece(1, 0, 0)
+
+    board.get_board_display()
+
+
+
+    # Perform the action of rotating the part 90° clockwise (2, 2)
+    # result_state = problem.result(initial_state, (2, 2, True))
+
+    # Show value at position (2, 2):
+    # print(result_state.board.get_value(2, 2))
